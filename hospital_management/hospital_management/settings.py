c62e61fcc33ddca1f2865575f4040e977c027d26
MerinaMou110 <122225168+MerinaMou110@users.noreply.github.com>
2025-04-22 23:59:57 +0600

    add alowed host

--- hospital_management/hospital_management/settings.py
+++ hospital_management/hospital_management/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://mediconnect-production-522d.up.railway.app','https://*.127.0.0.1']
 
 
 # Application definition
