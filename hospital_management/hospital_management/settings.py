fa818e388dcba16a1a9d0027d8d57752e54ed9f7
MerinaMou110 <122225168+MerinaMou110@users.noreply.github.com>
2025-04-23 02:12:03 +0600

    set allowed host

--- hospital_management/hospital_management/settings.py
+++ hospital_management/hospital_management/settings.py
@@ -33,7 +33,12 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://mediconnect-production-1675.up.railway.app','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'https://mediconnect-production-1675.up.railway.app',
+    'http://127.0.0.1:8000',  # or whatever your local port is
+    'http://localhost:8000',
+]
+
 
 
 # Application definition
